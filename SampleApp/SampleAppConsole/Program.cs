f831e7d7299d5942c9f688b5844ade801729c3d0
Harry Chou <harrychou@gmail.com>
2014-09-22 13:46:22 -0400

    first commit

---
+++ SampleApp/SampleAppConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hit any key to exit ....");
+            Console.ReadKey();
+        }
+    }
+}
